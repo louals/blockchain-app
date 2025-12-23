47fac9d49822418fa1850449a3818dca2034d6a0
louals <lou.als2904@gmail.com>
2025-12-22 20:46:24 -0500

    Added HashRouter to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { TransactionProvider } from "./context/TransactionContext.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <TransactionProvider>
-    <BrowserRouter basename="/">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </TransactionProvider>
 );
